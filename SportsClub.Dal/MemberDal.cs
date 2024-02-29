838499bba7ba4b91e207c78bdb5432bd60c1a067
Koenraad Pecceu <koenraad.pecceu@creo.be>
2024-02-29 10:33:05 +0100

    Add read all code to Dal

---
+++ SportsClub.Dal/MemberDal.cs
@@ -0,0 +1,32 @@
+﻿using SportsClub.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsClub.Dal
+{
+    public class MemberDal
+    {
+        // CREATE
+
+        // READ ALL
+        public List<Member> Read()
+        {
+            // verbinding met databank maken
+            using (var db = new SportsClubDbContext())
+            {
+                // lijst van members uit db halen
+                List<Member> lstMembers = db.Members.ToList();
+                return lstMembers;
+            }
+        }
+
+        // READ SINGLE
+
+        // UPDATE
+
+        // DELETE
+    }
+}
