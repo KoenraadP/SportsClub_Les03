aa254f769aab2cd3571275ef8e724daf5c58bcbc
Koenraad Pecceu <koenraad.pecceu@creo.be>
2024-02-29 11:36:55 +0100

    Add index for activities

---
+++ SportsClub.WebApp/Controllers/ActivitiesController.cs
@@ -0,0 +1,20 @@
+﻿using SportsClub.Bll;
+using SportsClub.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsClub.WebApp.Controllers
+{
+    public class ActivitiesController : Controller
+    {
+        // GET: Activities
+        public ActionResult Index()
+        {
+            List<Activity> lstActivities = Activities.Read();
+            return View(lstActivities);
+        }
+    }
+}
